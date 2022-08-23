34fed4f0c6f797f03a53798f584ee6b388dd0625
Sanmeet Wakchaure <wakchauresanmeet3@gmail.com>
2022-08-23 12:39:32 +0530

    Dark theme improved

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextBox from './Components/TextBox';
 // import About from './Components/About';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'dark') {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+    else {
+      setMode('dark');
+      document.body.style.backgroundColor = '#161616';
+    }
+  }
+
   return (
     <>
-    <Navbar title="TextUtils" nav1="Home" nav2="About" />
+    <Navbar title="TextUtils" nav1="Home" nav2="About" mode={mode} toggleMode={toggleMode} />
     {/* <Navbar title="TextUtils" /> */}
     {/* <Navbar /> */}
-    <TextBox heading="Enter the text below to analyze:"/>
+    <TextBox heading="Enter the text below to analyze:" mode={mode} />
     {/* <About /> */}
     </>
   );
